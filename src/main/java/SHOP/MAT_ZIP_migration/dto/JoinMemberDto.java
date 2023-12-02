33dfc521458c6c98581c5f87afb84af7ba043143
JayMon0327 <kioplm0211@naver.com>
2023-12-03 02:29:20 +0900

    회원가입 폼을 위한 회원가입 DTO 정의

---
+++ src/main/java/SHOP/MAT_ZIP_migration/dto/JoinMemberDto.java
@@ -0,0 +1,22 @@
+package SHOP.MAT_ZIP_migration.dto;
+
+import SHOP.MAT_ZIP_migration.domain.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class JoinMemberDto {
+
+    @NotBlank(message = "아이디는 빈칸일 수 없습니다.")
+    private String username;
+
+    @NotBlank(message = "비밀번호는 빈칸일 수 없습니다.")
+    private String password;
+
+    @Email
+    @NotBlank(message = "이메일은 빈칸일 수 없습니다.")
+    private String email;
+
+    private Role role;
+}
