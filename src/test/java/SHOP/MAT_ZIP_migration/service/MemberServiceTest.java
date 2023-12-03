3aae645ed4f8b01a62f0dfe726fde89654ff0b49
JayMon0327 <kioplm0211@naver.com>
2023-12-04 08:10:17 +0900

    회원가입 테스트와 회원 수정 테스트를 위한 TDD 작성

--- src/test/java/SHOP/MAT_ZIP_migration/service/MemberServiceTest.java
+++ src/test/java/SHOP/MAT_ZIP_migration/service/MemberServiceTest.java
@@ -1,38 +1,104 @@
 package SHOP.MAT_ZIP_migration.service;
 
 import SHOP.MAT_ZIP_migration.domain.Member;
-import SHOP.MAT_ZIP_migration.domain.Role;
 import SHOP.MAT_ZIP_migration.dto.JoinMemberDto;
+import SHOP.MAT_ZIP_migration.dto.UpdateMemberDto;
+import SHOP.MAT_ZIP_migration.exception.CustomErrorCode;
+import SHOP.MAT_ZIP_migration.exception.CustomException;
 import SHOP.MAT_ZIP_migration.repository.MemberRepository;
+import SHOP.MAT_ZIP_migration.service.validator.MemberValidator;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import static org.mockito.Mockito.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 
-@ExtendWith(MockitoExtension.class)
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
 class MemberServiceTest {
 
-    @Mock
-    private MemberRepository memberRepository;
-
-    @Mock
-    private PasswordEncoder passwordEncoder;
-
-    @InjectMocks
+    @Autowired
     private MemberService memberService;
+    @Autowired
+    private MemberRepository memberRepository;
+    @Autowired
+    private MemberValidator memberValidator;
+    @Autowired
+    private BCryptPasswordEncoder encoder;
 
+    @DisplayName("회원가입")
     @Test
-    void 회원가입_성공() {
-        JoinMemberDto joinMemberDto = new JoinMemberDto("testUser", "password", "test@example.com", Role.USER);
-        when(passwordEncoder.encode("password")).thenReturn("encodedPassword");
-        when(memberRepository.existsByUsername("testUser")).thenReturn(false);
-        when(memberRepository.existsByEmail("test@example.com")).thenReturn(false);
+    void signUp() {
+        JoinMemberDto joinMemberDto = new JoinMemberDto("kim", "testPassword", "testPassword", "kim@naver.com");
 
-        memberService.회원가입(joinMemberDto);
+        memberService.SignUp(joinMemberDto);
+        Member savedMember = memberRepository.findByUsername("kim");
 
-        verify(memberRepository).save(any(Member.class));
+        assertThat(savedMember.getUsername()).isEqualTo("kim");
+        assertThat(savedMember.getEmail()).isEqualTo("kim@naver.com");
+        assertThat(encoder.matches("testPassword", savedMember.getPassword())).isTrue();
+    }
+
+    @DisplayName("비밀번호 확인과 예외")
+    @Test
+    void passwordCheck() {
+        JoinMemberDto joinMemberDto = new JoinMemberDto("kim", "testPassword", "testPassword", "kim@naver.com");
+        String rawPassword = joinMemberDto.getPassword();
+        String passwordCheck = "testPassword";
+        String encodedPassword = memberValidator.PasswordCheck(rawPassword, passwordCheck);
+
+        assertThat(encoder.matches(rawPassword, encodedPassword)).isTrue();
+        assertThatThrownBy(() -> memberValidator.PasswordCheck(joinMemberDto.getPassword(), "failPassword"))
+                .isInstanceOf(CustomException.class);
+    }
+
+    @DisplayName("중복 유저네임 예외")
+    @Test
+    void DuplicateUsername() {
+        JoinMemberDto joinMemberDto1 = new JoinMemberDto("kim", "testPassword", "testPassword", "kim@naver.com");
+        JoinMemberDto joinMemberDto2 = new JoinMemberDto("kim", "test", "test", "Lee@naver.com");
+        memberService.SignUp(joinMemberDto1);
+
+        assertThatThrownBy(() -> memberService.SignUp(joinMemberDto2))
+                .isInstanceOf(CustomException.class)
+                .hasFieldOrPropertyWithValue("errorCode", CustomErrorCode.USERNAME_ALREADY_EXISTS.getErrorCode())
+                .hasFieldOrPropertyWithValue("errorMessage", CustomErrorCode.USERNAME_ALREADY_EXISTS.getErrorMessage());
+    }
+
+    @DisplayName("중복 이메일 예외")
+    @Test
+    void DuplicateEmail() {
+        JoinMemberDto joinMemberDto1 = new JoinMemberDto("kim", "testPassword", "testPassword", "kim@naver.com");
+        JoinMemberDto joinMemberDto2 = new JoinMemberDto("LEE", "test", "test", "kim@naver.com");
+        memberService.SignUp(joinMemberDto1);
+
+        assertThatThrownBy(() -> memberService.SignUp(joinMemberDto2))
+                .isInstanceOf(CustomException.class)
+                .hasFieldOrPropertyWithValue("errorCode", CustomErrorCode.EMAIL_ALREADY_EXISTS.getErrorCode())
+                .hasFieldOrPropertyWithValue("errorMessage", CustomErrorCode.EMAIL_ALREADY_EXISTS.getErrorMessage());
+    }
+
+    @DisplayName("회원정보 수정")
+    @Test
+    void updateEmailAndPassword() {
+        JoinMemberDto joinMemberDto = new JoinMemberDto("kim", "testPassword", "testPassword", "kim@naver.com");
+        memberService.SignUp(joinMemberDto);
+        Member savedMember = memberRepository.findByUsername("kim");
+
+        UpdateMemberDto updateMemberDto = new UpdateMemberDto(savedMember.getId(), "kim", "replacePassword",
+                "replacePassword", "LEE@naver.com");
+        memberService.update(updateMemberDto);
+
+
+        assertThat(savedMember.getEmail()).isEqualTo("LEE@naver.com");
+        assertThat(encoder.matches("replacePassword", savedMember.getPassword())).isTrue();
     }
 }
