e1a34e253c28dccd1cb88fbf9803bbc8a09781b6
JayMon0327 <kioplm0211@naver.com>
2023-12-06 18:34:36 +0900

    상품 등록 통합 컨트롤러 코드 작성     - 응답값으로 상품 게시판 id 값 추가

--- src/main/java/SHOP/MAT_ZIP_migration/controller/api/ProductApiController.java
+++ src/main/java/SHOP/MAT_ZIP_migration/controller/api/ProductApiController.java
@@ -3,8 +3,11 @@ package SHOP.MAT_ZIP_migration.controller.api;
 import SHOP.MAT_ZIP_migration.config.auth.PrincipalDetails;
 import SHOP.MAT_ZIP_migration.domain.Product;
 import SHOP.MAT_ZIP_migration.dto.ResponseDto;
+import SHOP.MAT_ZIP_migration.dto.product.RequestItemDto;
 import SHOP.MAT_ZIP_migration.dto.product.RequestProductDto;
+import SHOP.MAT_ZIP_migration.service.ItemService;
 import SHOP.MAT_ZIP_migration.service.ProductService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -12,6 +15,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,14 +26,16 @@ public class ProductApiController {
     private final ProductService productService;
 
     @PostMapping("/product")
-    public ResponseDto<Integer> save(@RequestBody RequestProductDto requestProductDto, @AuthenticationPrincipal PrincipalDetails principal) throws IOException {
-        productService.save(requestProductDto, principal.getMember());
-        return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
+    public ResponseDto<Integer> save(@Valid @RequestBody RequestProductDto requestProductDto,
+                                     @Valid @RequestBody List<RequestItemDto> requestItemDtos,
+                                     @AuthenticationPrincipal PrincipalDetails principal) throws IOException {
+        Long productId = productService.saveProductAndItem(requestProductDto, requestItemDtos, principal.getMember());
+        return new ResponseDto<Integer>(HttpStatus.OK.value(),1, productId);
     }
 
     @PutMapping("/product/{id}")
     public ResponseDto<Integer> update(@PathVariable Long id, @RequestBody RequestProductDto requestProductDto) throws IOException {
-        productService.update(id, requestProductDto);
+        productService.updateProduct(id, requestProductDto);
         return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
     }
 
