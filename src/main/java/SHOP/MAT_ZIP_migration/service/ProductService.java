b25c7a3757d24317a2ef3d85301641c5b084b8cf
JayMon0327 <kioplm0211@naver.com>
2023-12-07 22:55:40 +0900

    상세 페이지 응답 DTO에 값 담아주는 메서드 작성

--- src/main/java/SHOP/MAT_ZIP_migration/service/ProductService.java
+++ src/main/java/SHOP/MAT_ZIP_migration/service/ProductService.java
@@ -7,8 +7,10 @@ import SHOP.MAT_ZIP_migration.domain.order.Item;
 import SHOP.MAT_ZIP_migration.dto.product.ProductAndItemDto;
 import SHOP.MAT_ZIP_migration.dto.product.RequestItemDto;
 import SHOP.MAT_ZIP_migration.dto.product.RequestProductDto;
+import SHOP.MAT_ZIP_migration.dto.product.ResponseProductDto;
 import SHOP.MAT_ZIP_migration.repository.ItemRepository;
 import SHOP.MAT_ZIP_migration.repository.ProductRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -18,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -41,11 +44,12 @@ public class ProductService {
     }
 
     @Transactional
-    public void updateProductAndItem(Long id, ProductAndItemDto dto) throws IOException {
+    public Long updateProductAndItem(Long id, ProductAndItemDto dto) throws IOException {
         Long productId = updateProduct(id, dto.getProductDto());
         for (RequestItemDto requestItemDto : dto.getItemDtos()) {
             itemService.saveItem(productId, requestItemDto);
         }
+        return productId;
     }
 
     @Transactional
@@ -85,6 +89,32 @@ public class ProductService {
         return productRepository.findById(id).orElseThrow(()->{
             return new IllegalArgumentException("상세보기 실패: 상품 아이디가 존재하지 않습니다");
         });
+    }
+
+    public ResponseProductDto getProductDetails(Long productId) {
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new EntityNotFoundException("상품을 찾을 수 없습니다."));
+
+        // ProductDetailDto에 정보 채우기
+        ResponseProductDto dto = new ResponseProductDto();
+        dto.setId(product.getId());
+        dto.setTitle(product.getTitle());
+        dto.setDescription(product.getDescription());
+        dto.setMember(product.getMember());
+
+        // Product의 Item 리스트를 DTO로 변환
+        List<ResponseProductDto.ItemDto> itemDtos = product.getItems().stream()
+                .map(item -> new ResponseProductDto.ItemDto(item.getName(), item.getPrice(), item.getStockQuantity()))
+                .collect(Collectors.toList());
+        dto.setItems(itemDtos);
+
+        // Product의 Image 리스트를 DTO로 변환
+        List<ResponseProductDto.ImageDto> imageDtos = product.getProductImages().stream()
+                .map(image -> new ResponseProductDto.ImageDto(image.getStoreFileName()))
+                .collect(Collectors.toList());
+        dto.setImages(imageDtos);
+
+        return dto;
     }
 
     public Page<Product> productAll(Pageable pageable) {
