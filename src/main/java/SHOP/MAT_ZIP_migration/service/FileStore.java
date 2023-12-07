3a2678c5ab0f28cb0cd97a40a1a9045d142026de
JayMon0327 <kioplm0211@naver.com>
2023-12-08 05:33:11 +0900

    파일 업로드를 위해 리뷰 이미지 파일명 변환 메서드 추가

--- src/main/java/SHOP/MAT_ZIP_migration/service/FileStore.java
+++ src/main/java/SHOP/MAT_ZIP_migration/service/FileStore.java
@@ -1,6 +1,7 @@
 package SHOP.MAT_ZIP_migration.service;
 
 import SHOP.MAT_ZIP_migration.domain.ProductImage;
+import SHOP.MAT_ZIP_migration.domain.ReviewImage;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,17 +18,17 @@ public class FileStore {
     @Value("${file.dir}")
     private String fileDir;
 
-    public List<ProductImage> storeFiles(List<MultipartFile> multipartFiles) throws IOException {
+    public List<ProductImage> storeProductFiles(List<MultipartFile> multipartFiles) throws IOException {
         List<ProductImage> storeFileResult = new ArrayList<>();
         for (MultipartFile multipartFile : multipartFiles) {
             if (!multipartFile.isEmpty()) {
-                storeFileResult.add(storeFile(multipartFile));
+                storeFileResult.add(storeProductFile(multipartFile));
             }
         }
         return storeFileResult;
     }
 
-    private ProductImage storeFile(MultipartFile multipartFile) throws IOException {
+    private ProductImage storeProductFile(MultipartFile multipartFile) throws IOException {
         String uploadFileName = multipartFile.getOriginalFilename();
         String storeFileName = createStoreFileName(uploadFileName);
         multipartFile.transferTo(new File(getFullPath(storeFileName)));
@@ -35,6 +36,26 @@ public class FileStore {
         ProductImage productImage = new ProductImage();
         productImage.addFile(uploadFileName,storeFileName, getFullPath(storeFileName));
         return productImage;
+    }
+
+    public List<ReviewImage> storeReviewFiles(List<MultipartFile> multipartFiles) throws IOException {
+        List<ReviewImage> storeFileResult = new ArrayList<>();
+        for (MultipartFile multipartFile : multipartFiles) {
+            if (!multipartFile.isEmpty()) {
+                storeFileResult.add(storeReviewFile(multipartFile));
+            }
+        }
+        return storeFileResult;
+    }
+
+    private ReviewImage storeReviewFile(MultipartFile multipartFile) throws IOException {
+        String uploadFileName = multipartFile.getOriginalFilename();
+        String storeFileName = createStoreFileName(uploadFileName);
+        multipartFile.transferTo(new File(getFullPath(storeFileName)));
+
+        ReviewImage reviewImage = new ReviewImage();
+        reviewImage.addFile(uploadFileName,storeFileName, getFullPath(storeFileName));
+        return reviewImage;
     }
 
     private String createStoreFileName(String originalFilename) {
