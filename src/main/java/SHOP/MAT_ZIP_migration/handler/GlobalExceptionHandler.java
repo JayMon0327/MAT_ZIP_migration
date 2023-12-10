b909ee9c045decc31d050c60533f13ff779a597b
JayMon0327 <kioplm0211@naver.com>
2023-12-11 06:49:36 +0900

    errors.properties 오류 메시지 전역 예외처리 설정

--- src/main/java/SHOP/MAT_ZIP_migration/handler/GlobalExceptionHandler.java
+++ src/main/java/SHOP/MAT_ZIP_migration/handler/GlobalExceptionHandler.java
@@ -4,14 +4,18 @@ import SHOP.MAT_ZIP_migration.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.validation.ObjectError;
-import java.util.List;
-import java.util.stream.Collectors;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
@@ -20,23 +24,35 @@ public class GlobalExceptionHandler {
 
     private final MessageSource messageSource;
 
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    public ResponseEntity handleValidationExceptions(MethodArgumentNotValidException ex) {
+//        String errorMessage = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+//        log.info("검증에러 캐치 " + errorMessage);
+//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+//    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity handleValidationExceptions(MethodArgumentNotValidException ex) {
-        String errorMessage = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
-        log.info("검증에러 캐치 " + errorMessage);
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = (error instanceof FieldError) ? ((FieldError) error).getField() : error.getObjectName();
+            String errorMessage = getErrorMessage(error);
+            errors.put(fieldName, errorMessage);
+        });
+
+        log.info("검증 오류 발생: " + errors);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    public ResponseEntity<List<String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
-//        List<String> errorMessages = ex.getBindingResult().getAllErrors().stream()
-//                .map(ObjectError::getDefaultMessage)
-//                .map(code -> messageSource.getMessage(code, null, LocaleContextHolder.getLocale()))
-//                .collect(Collectors.toList());
-//
-//        log.info("검증 오류 발생: " + errorMessages);
-//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessages);
-//    }
+    private String getErrorMessage(ObjectError error) {
+        String[] codes = error.getCodes();
+        for (String code : codes) {
+            try {
+                return messageSource.getMessage(code, error.getArguments(), Locale.KOREA);
+            } catch (NoSuchMessageException ignored) {}
+        }
+        return error.getDefaultMessage();
+    }
 
 
     @ExceptionHandler(CustomException.class)
@@ -50,5 +66,4 @@ public class GlobalExceptionHandler {
         log.info("IllegalArgument 캐치 " + ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
-
 }
