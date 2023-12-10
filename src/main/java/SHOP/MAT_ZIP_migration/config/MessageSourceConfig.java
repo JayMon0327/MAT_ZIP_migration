9da69de2f12bf395aaad5434872afaa03b07bdfb
JayMon0327 <kioplm0211@naver.com>
2023-12-11 06:48:50 +0900

    application.yml 의 spring messages 설정 미지원 문제로 직접 messageSourceConfig 파일을 만들어 수동 빈 등록했음

---
+++ src/main/java/SHOP/MAT_ZIP_migration/config/MessageSourceConfig.java
@@ -0,0 +1,21 @@
+package SHOP.MAT_ZIP_migration.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+/**
+ * application.yml의 messages 설정 버그로 인한 전통적인 Config파일 생성
+ */
+@Configuration
+public class MessageSourceConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:errors");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+}
