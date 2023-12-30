7c349a756813d7d2a5b198e0bc6d2dff5906ffc9
JayMon0327 <kioplm0211@naver.com>
2023-12-30 23:25:16 +0900

    war배포를 위한 메인클래스 SpringBootServletInitializer 상속

--- src/main/java/SHOP/MAT_ZIP_migration/MatZipMigrationApplication.java
+++ src/main/java/SHOP/MAT_ZIP_migration/MatZipMigrationApplication.java
@@ -2,14 +2,21 @@ package SHOP.MAT_ZIP_migration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class MatZipMigrationApplication {
+public class MatZipMigrationApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MatZipMigrationApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MatZipMigrationApplication.class);
     }
 
 }
